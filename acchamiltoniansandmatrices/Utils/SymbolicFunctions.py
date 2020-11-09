9f9ac3799f44dfb59daa66789c01d55e50375222
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2020-11-10 00:23:35 +0100

    further updating symbolic calculations activated blackboard in rise activated scroll in rise

--- acchamiltoniansandmatrices/Utils/SymbolicFunctions.py
+++ acchamiltoniansandmatrices/Utils/SymbolicFunctions.py
@@ -1,4 +1,4 @@
-from sympy import NumberSymbol, latex
+from sympy import Derivative, Eq, NumberSymbol, latex, solve, symbols
 
 
 def pla(e):
@@ -24,3 +24,20 @@ class SymbolTrick(NumberSymbol):
 
     def _latex(self, printer, *args):
         return r"{}".format(self._name)
+
+
+def fixedpoints2D(ham, x, px):
+    partialderiv_x = Derivative(ham, x)
+    partialderiv_p = Derivative(ham, px)
+    H_x = partialderiv_x.doit()
+    H_p = partialderiv_p.doit()
+
+    # Using the full drift Hamiltonian requires more computing power therefore takes much more time
+    # Should implement parallel computing i.e. run on all cores
+
+    x_FP = Eq(H_x, 0)  # dH/dx = 0
+    p_FP = Eq(H_p, 0)  # dH/dp = 0
+
+    # compute fixed points
+    fixed_points = solve((x_FP, p_FP), x, px)
+    return fixed_points
