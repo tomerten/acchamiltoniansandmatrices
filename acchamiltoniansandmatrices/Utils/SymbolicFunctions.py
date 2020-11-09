93fc072bca73778df7805b869d449712853b196f
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2020-11-09 15:17:34 +0100

    added symboltrick method for having number symbols in the symbolic calculations

--- acchamiltoniansandmatrices/Utils/SymbolicFunctions.py
+++ acchamiltoniansandmatrices/Utils/SymbolicFunctions.py
@@ -1,4 +1,4 @@
-from sympy import latex
+from sympy import NumberSymbol, latex
 
 
 def pla(e):
@@ -7,3 +7,20 @@ def pla(e):
     creation of this notebook.
     """
     print(latex(e))
+
+
+class SymbolTrick(NumberSymbol):
+    """
+    https://stackoverflow.com/questions/39665207/define-a-variable-in-sympy-to-be-a-constant
+    """
+
+    def __new__(self, name):
+        obj = NumberSymbol.__new__(self)
+        obj._name = name
+        return obj
+
+    __str__ = lambda self: str(self._name)
+    _as_mpf_val = 1.0
+
+    def _latex(self, printer, *args):
+        return r"{}".format(self._name)
