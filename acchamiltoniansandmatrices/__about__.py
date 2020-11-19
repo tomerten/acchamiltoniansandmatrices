f8d1def5e04a3789e5b5a8d8527f1c4b53bc1750
Tom Mertens <tom.mertens@helmholtz-berlin.de>
2020-11-19 12:30:44 +0100

    updated author list

--- acchamiltoniansandmatrices/__about__.py
+++ acchamiltoniansandmatrices/__about__.py
@@ -2,5 +2,5 @@ __title__ = "acchamiltoniansandmatrices"
 __description__ = "Collection of Accelerator Physics Hamiltonians and Matrices"
 __url__ = ("https://github.com/tomerten/acchamiltoniansandmatrices",)
 __version__ = "1.0.1a0"
-__author__ = "Tom Mertens"
+__author__ = "Tom Mertens, Jernej Frank, Michael Arlandoo"
 __license__ = "GNU General Public License v3.0"
